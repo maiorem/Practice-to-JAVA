7f2da5809afff938a61b78f59f152d0eea78c977
a4-01 <maiorem00@gmail.com>
2020-04-08 18:28:12 +0900

    Create FifteenOutput.java

---
+++ java_project/FirstJava/src/chapter1/FifteenOutput.java
@@ -0,0 +1,14 @@
+package chapter1;
+
+public class FifteenOutput {
+
+	public static void main(String[] args) {
+		System.out.println(15);
+		System.out.println(3+12);
+		System.out.println(3*5);
+		System.out.println(45/3);
+		System.out.println("1"+"5");
+
+	}
+
+}
