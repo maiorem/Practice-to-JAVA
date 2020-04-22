5b55faa5539bac3f36fc04dc509081c3b7f3a774
a4-01 <maiorem00@gmail.com>
2020-04-22 10:13:08 +0900

    Update StringEx2.java

--- java_project/Assignment/src/ClassAndInstance/StringEx2.java
+++ java_project/Assignment/src/ClassAndInstance/StringEx2.java
@@ -7,7 +7,6 @@ public class StringEx2 {
 		String str = "990929-1010123";
 		StringBuffer str1 = new StringBuffer(str);
 		System.out.println(str1.deleteCharAt(6));
-		String str_d = new String(str1); // 새 인스턴스 생성?
 
 	}
 
