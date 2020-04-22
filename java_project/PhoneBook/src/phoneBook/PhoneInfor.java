f940a151d680c0784b47b74ca26ea0f4d2fa0442
a4-01 <maiorem00@gmail.com>
2020-04-22 09:29:22 +0900

    전화번호 수정

--- java_project/PhoneBook/src/phoneBook/PhoneInfor.java
+++ java_project/PhoneBook/src/phoneBook/PhoneInfor.java
@@ -1,53 +1,36 @@
 package phoneBook;
 
+import java.util.Scanner;
 
 public class PhoneInfor {
 
 	String name;		//이름 
 	String phoneNumber;	//전화번호
 	String birthday;	//생년월일
+	Scanner kb = new Scanner(System.in);	
 	
 	
 	PhoneInfor(String name, String number, String day) {
-		
 		this.name = name;
 		phoneNumber = number;
 		birthday = day;
-		
+
 	}
 	
 	PhoneInfor(String name, String number) {
 		this(name, number, "--");		
 	}
 	
-	public PhoneInfor() {}
-
-	@Override
-	public String toString() {
-		return "PhoneInfor [name=" + name + ", phoneNumber=" + phoneNumber + ", birthday=" + birthday + "]";
-	}
 	
-	public void showInfor() {
-		System.out.println("이름 : "+name);
-		System.out.println("전화번호 : "+phoneNumber);
-		System.out.println("생년월일 : "+birthday);
-	}
-		
 	
-	PhoneInfor[] p = new PhoneInfor[100];
-
-	public void searchArr(String name) {
-
-		for (int i=0; i<p.length; i++) {
-			if(p[i].name==name) {
-				p[i].showInfor();
-			}
+	void search(String name) { 
+		if (this.name==name) {
+			System.out.println("이름 : "+this.name);
+			System.out.println("전화번호 : "+phoneNumber);
+			System.out.println("생년월일 : "+birthday);
+			
 		}
-	
 	}
-
-
-
 
 }
 
--- java_project/PhoneBook/src/phoneBook/PhoneMain.java
+++ java_project/PhoneBook/src/phoneBook/PhoneMain.java
@@ -8,41 +8,47 @@ public class PhoneMain {
 	
 		Scanner kb = new Scanner(System.in);
 		PhoneInfor[] phone = new PhoneInfor[100];
+
+		int num = 0;
 		
 		for(int i=0; i<phone.length; i++) {
 			
-			System.out.println("무엇을 할까요?");
+			System.out.println("무엇을 하시겠습니까?");
 			System.out.println("1.저장  2.검색  3.삭제  4.종료");
-			int num = kb.nextInt();
+			num = kb.nextInt();
 			
-			if (num==1) {
-			
-				System.out.print("이름을 입력하세요 : ");
+			if (num==1) {			
+				
+				System.out.println("이름을 입력하세요 : ");
 				phone[i].name = kb.nextLine();
-				System.out.print("전화번호를 입력하세요 : ");
+				System.out.println("전화번호를 입력하세요 : ");
 				phone[i].phoneNumber = kb.nextLine();
-				System.out.print("생년월일을 입력하세요 : ");
-				phone[i].birthday  = kb.nextLine();
+				System.out.println("생년월일을 입력하세요 : ");
+				phone[i].birthday = kb.nextLine();
 				
-				if (phone[i].birthday == null) {
-					phone[i] = new PhoneInfor (phone[i].name, phone[i].phoneNumber);
-					continue;
-				} else {
-					phone[i] = new PhoneInfor (phone[i].name, phone[i].phoneNumber, phone[i].birthday);
-					continue;
-				}
-
-			} else if (num==2) {
-		
+				phone[i] = new PhoneInfor (phone[i].name, phone[i].phoneNumber, phone[i].birthday);
 				continue;
-			} else if (num==3) {
 				
+			}
+			else if (num==2) {
+				System.out.println("검색할 이름을 입력하세요 : ");
+				phone[i].name = kb.nextLine();
+				phone[i].search(phone[i].name);
 				continue;
-			} else if (num==4) {
+			}
+			
+			
+//			else if (num==3) {
+//				System.out.println("삭제할 이름을 입력하세요 : ");
+//				name = kb.nextLine();
+//				continue;
+//			}
+			
+			else if (num==4) {
 				break;
 			}
+
 		}
-			
 			
 	}
 }
