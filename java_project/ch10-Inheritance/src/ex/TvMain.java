e9fd4482d4ff14bdfef5151067dcbc3dc5ce844f
a4-01 <maiorem00@gmail.com>
2020-04-23 17:32:38 +0900

    다형성 형변환

--- java_project/ch10-Inheritance/src/ex/TvMain.java
+++ java_project/ch10-Inheritance/src/ex/TvMain.java
@@ -4,9 +4,26 @@ public class TvMain {
 
 	public static void main(String[] args) {
 		Tv tv1 = new Tv();
+		
 		CaptionTv cTv1 = new CaptionTv();
 		
 		Tv tv2 = new CaptionTv();
+		
+		//형변환 (상위 -> 하위)
+		CaptionTv cTv2 = (CaptionTv) tv2;
+//		((CaptionTv) tv2).caption();
+		cTv2.display();
+		
+		//하위클래스에서 상위클래스 형변환은 자동.
+		Tv t = cTv1;
+
+		
+//		IpTv itv = (IpTv) tv1; // 에러
+//		IpTv itv2 = (IpTv) tv2; // => 명시해도 형변환이 안되는 경우.
+//		s
+//		itv.power(); // 에러
+//		itv2.display(); // => 에러.
+		
 //		System.out.println(tv2.power);
 		tv2.power();
 //		System.out.println(tv2.power);
@@ -25,9 +42,9 @@ public class TvMain {
 		tvs[2] = new IpTv();
 		tvs[3] = new SmartTv();
 		
-		for(int i=0;i<tvs.length; i++) {
-			tvs[i].display();
+//		for(int i=0;i<tvs.length; i++) {
+//			tvs[i].display();
 		}
 		
 	}
-}
+
