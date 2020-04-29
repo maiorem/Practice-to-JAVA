9c6f0d77321876b0a1489bd06841408ebe872aef
a4-01 <maiorem00@gmail.com>
2020-04-29 12:48:21 +0900

    Update PhoneBookManager.java

--- java_project/PhoneBook/src/ver06/PhoneBookManager.java
+++ java_project/PhoneBook/src/ver06/PhoneBookManager.java
@@ -28,7 +28,7 @@ public class PhoneBookManager {
 	//공백 입력시 예외처리할 메서드
 	String readString() throws StringInputNoneException{
 		String str=sc.nextLine();
-		if (!(str.trim().isEmpty())) {
+		if (str.trim().isEmpty()) {
 			StringInputNoneException except = new StringInputNoneException();
 			throw except;
 		}
