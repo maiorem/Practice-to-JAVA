70c4718051467748325160d1935015ddeca2bf67
Hong Seyoung <maiorem00@gmail.com>
2020-07-03 18:21:26 +0900

    해쉬맵 처리중

---
+++ jsp_project/OpenProject/src/model/MemberManager.java
@@ -0,0 +1,21 @@
+package model;
+
+import java.util.HashMap;
+
+public class MemberManager {
+
+	
+	HashMap<String, MemberInfo> info=new HashMap<>(); 
+	
+	
+	
+	
+	public MemberInfo createMember() {
+		MemberInfo info=null;
+		
+		
+		return info;
+	}
+	
+	
+}
