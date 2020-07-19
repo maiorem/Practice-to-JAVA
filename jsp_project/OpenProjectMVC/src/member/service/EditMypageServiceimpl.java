c9ef5ab25ccc807d508e015019ef60aca7e6ae85
maiorem <maiorem00@gmail.com>
2020-07-19 20:32:14 +0900

    mypage delete edit

---
+++ jsp_project/OpenProjectMVC/src/member/service/EditMypageServiceimpl.java
@@ -0,0 +1,16 @@
+package member.service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import service.Service;
+
+public class EditMypageServiceimpl implements Service {
+
+	@Override
+	public String getViewPage(HttpServletRequest req, HttpServletResponse resp) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
