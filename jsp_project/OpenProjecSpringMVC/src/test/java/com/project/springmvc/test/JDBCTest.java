32ed6f9dc64ba70b1a347da37012feca60fb6775
maiorem <maiorem00@gmail.com>
2020-08-04 11:34:39 +0900

    junit test?

---
+++ jsp_project/OpenProjecSpringMVC/src/test/java/com/project/springmvc/test/JDBCTest.java
@@ -0,0 +1,29 @@
+package com.project.springmvc.test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/*.xml")
+public class JDBCTest {
+
+	@Autowired
+	private DataSource dataSource;
+	
+	@Test
+	public void connectionTest() throws SQLException {
+		System.out.println("dataSource ========> " +dataSource);
+		
+		Connection conn=null;
+		conn=dataSource.getConnection();
+		System.out.println("Connection =========> "+conn);
+	}
+}
