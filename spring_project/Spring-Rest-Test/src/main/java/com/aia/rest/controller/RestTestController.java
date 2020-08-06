8f7a5fd1c9882c423ff93da0c9397027bd9ee0bc
maiorem <maiorem00@gmail.com>
2020-08-06 17:09:52 +0900

    rest list map

--- spring_project/Spring-Rest-Test/src/main/java/com/aia/rest/controller/RestTestController.java
+++ spring_project/Spring-Rest-Test/src/main/java/com/aia/rest/controller/RestTestController.java
@@ -1,5 +1,11 @@
 package com.aia.rest.controller;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +15,33 @@ import com.aia.rest.model.Member;
 @RestController
 @RequestMapping("/rest/v1")
 public class RestTestController {
+	
+	@RequestMapping("/membermap")
+	public Map<Integer, Member> getMap(){
+		
+		Map<Integer, Member> map=new HashMap<Integer, Member>();
+		
+		map.put(1,new Member(1, "cool1", "cool1@gmail.com", "cool1.png", new Date()));
+		map.put(2,new Member(2, "cool2", "cool2@gmail.com", "cool2.png", new Date()));
+		map.put(3,new Member(3, "cool3", "cool3@gmail.com", "cool3.png", new Date()));
+		map.put(4,new Member(4, "cool4", "cool4@gmail.com", "cool4.png", new Date()));
+		map.put(5,new Member(5, "cool5", "cool5@gmail.com", "cool5.png", new Date()));
+		map.put(6,new Member(6, "cool6", "cool6@gmail.com", "cool6.png", new Date()));
+		
+		return map;
+	}
+	
+	@RequestMapping("/memberlist")
+	public List<Member> getList(){
+		List<Member> list=new ArrayList<Member>();
+		list.add(new Member(1, "cool1", "cool1@gmail.com", "cool1.png", new Date()));
+		list.add(new Member(2, "cool2", "cool2@gmail.com", "cool2.png", new Date()));
+		list.add(new Member(3, "cool3", "cool3@gmail.com", "cool3.png", new Date()));
+		list.add(new Member(4, "cool4", "cool4@gmail.com", "cool4.png", new Date()));
+		list.add(new Member(5, "cool5", "cool5@gmail.com", "cool5.png", new Date()));
+		list.add(new Member(6, "cool6", "cool6@gmail.com", "cool6.png", new Date()));
+		return list;
+	}
 	
 	//== RequestMapping(method=RequestMethod.GET)
 	@GetMapping
--- spring_project/Spring-Rest-Test/src/main/java/com/aia/rest/model/Member.java
+++ spring_project/Spring-Rest-Test/src/main/java/com/aia/rest/model/Member.java
@@ -1,12 +1,33 @@
 package com.aia.rest.model;
 
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 public class Member {
 
 	private Integer id; 
 	private String name; 
+	@JsonIgnore
 	private String email; 
 	private String photo; 
-	
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone="Asia/Seoul")
+	private Date regdate;
+
+	public Member() {
+		
+	}
+
+	public Member(Integer id, String name, String email, String photo, Date regdate) {
+
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.photo = photo;
+		this.regdate = regdate;
+	}
+
 	public Integer getId() { return id; }
 
 	public String getName() {
@@ -35,6 +56,14 @@ public class Member {
 
 	public void setId(Integer id) {
 		this.id = id;
+	}
+
+	public Date getRegdate() {
+		return regdate;
+	}
+
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
 	}
 
 	
