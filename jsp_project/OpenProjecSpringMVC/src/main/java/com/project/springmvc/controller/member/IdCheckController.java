b8a6136bab3a529f83a4b8874c1a4b437985869b
maiorem <maiorem00@gmail.com>
2020-08-07 09:24:55 +0900

    Update IdCheckController.java

--- jsp_project/OpenProjecSpringMVC/src/main/java/com/project/springmvc/controller/member/IdCheckController.java
+++ jsp_project/OpenProjecSpringMVC/src/main/java/com/project/springmvc/controller/member/IdCheckController.java
@@ -1,10 +1,23 @@
 package com.project.springmvc.controller.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.project.springmvc.service.member.IdCheckService;
 
 @Controller
-@RequestMapping("/member/idCheck")
 public class IdCheckController {
+	
+	@Autowired
+	IdCheckService idService;
+	
+	@RequestMapping
+	@ResponseBody
+	public String idCheck(@RequestParam("email") String email) {
+		return idService.idcheck(email);
+	}
 
 }
