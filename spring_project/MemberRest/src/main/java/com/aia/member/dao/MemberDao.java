a7c829cb4eda14c075e1937673c998b24424bfbc
maiorem <maiorem00@gmail.com>
2020-08-24 15:52:37 +0900

    spring boot

---
+++ spring_project/MemberRest/src/main/java/com/aia/member/dao/MemberDao.java
@@ -0,0 +1,20 @@
+package com.aia.member.dao;
+
+import java.util.List;
+
+import com.aia.member.model.Member;
+
+
+public interface MemberDao {
+
+	List<Member> selectList();
+
+	int insertMember(Member member);
+
+	Member selectByIdx(int idx);
+
+	int editMember(Member member);
+
+	int deleteMember(int idx);
+
+}
---
+++ spring_project/MemberRest/src/main/java/com/aia/member/service/MemberViewService.java
@@ -0,0 +1,31 @@
+package com.aia.member.service;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.aia.member.dao.MemberDao;
+import com.aia.member.model.Member;
+
+@Service
+public class MemberViewService {
+	
+	private MemberDao dao;
+	
+	@Autowired
+	private SqlSessionTemplate template;
+	
+	public Member getMember(int idx) {
+		
+		dao = template.getMapper(MemberDao.class);
+		
+		return dao.selectByIdx(idx);
+	}
+	
+	
+	
+	
+	
+	
+
+}
